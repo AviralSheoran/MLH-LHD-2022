99486756372172370a463418e5e1977db01ca836
AviralSheoran <aviralsheoran.2002@gmail.com>
2022-01-11 14:55:46 +0530

    Add the program to reverse a string

--- script-to-reverse-a-string.cpp
+++ script-to-reverse-a-string.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main () {
 
+    string str = "Local Hack Day";
+ 
+    // Reverse str[begin..end]
+    reverse(str.begin(), str.end());
+ 
+    cout << str << endl;
 
 
     return 0;
